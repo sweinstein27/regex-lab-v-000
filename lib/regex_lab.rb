f7a6872c78e3a23c7da3376ef52d3bfc4f2e6347
Scott Weinstein <sweinstein27@gmail.com>
2018-04-24 16:39:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,16 +12,16 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  if text.match(/^[A-Z].+[\.!?]$//)
+
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].+[\.!?]$/)
    return true
    else
     return false
    end
   end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
 
 def valid_phone_number?(phone)
 
